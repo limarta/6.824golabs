dddad4646f09556b794825d919983d001d4f7036
Ian Limarta <limarta@mit.edu>
2022-02-25 17:08:46 -0500

    Richer logging tools

--- src/raft/util.go
+++ src/raft/util.go
@@ -5,26 +5,33 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
 type logTopic string
 
 const (
-	dInit    logTopic = "INIT"
-	dLeader  logTopic = "LEAD"
-	dStart   logTopic = "START"
-	dReqVote logTopic = "REQVOTE"
-	dTimer   logTopic = "TIMER"
-	dElect   logTopic = "ELECT"
-	dBeat    logTopic = "BEAT"
-	dTick    logTopic = "TICK"
-	dWon     logTopic = "WON"
+	dInit         logTopic = "INIT"
+	dLeader       logTopic = "LEAD"
+	dStart        logTopic = "START"
+	dReqVote      logTopic = "REQVOTE"
+	dTimer        logTopic = "TIMER"
+	dElect        logTopic = "ELECT"
+	dBeat         logTopic = "BEAT"
+	dTick         logTopic = "TICK"
+	dWon          logTopic = "WON"
+	dAppend       logTopic = "AE"
+	dAppendListen logTopic = "AL"
+	dDemote       logTopic = "DEMOTE"
+	dCommit       logTopic = "COMMIT"
+	dApply        logTopic = "APPLY"
 )
 
 // Debugging
 const Debug = false
 
 var debugVerbosity int
+var debugStart time.Time
 
 func setVerbosity() int {
 	v := os.Getenv("VERBOSE")
@@ -37,6 +44,7 @@ func setVerbosity() int {
 		}
 	}
 	debugVerbosity = level
+	debugStart = time.Now()
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 	return level
 }
@@ -47,7 +55,12 @@ func resetVerbosity() int {
 }
 
 func DPrintf(dTopic logTopic, format string, a ...interface{}) (n int, err error) {
-	if debugVerbosity >= 1 {
+	if debugVerbosity == 1 {
+		time := time.Since(debugStart).Microseconds()
+		prefix := fmt.Sprintf("%06d %-7v ", time, string(dTopic))
+		format = prefix + format
+		log.Printf(format, a...)
+	} else if debugVerbosity == 2 {
 		prefix := fmt.Sprintf("%-7v ", string(dTopic))
 		format = prefix + format
 		log.Printf(format, a...)
