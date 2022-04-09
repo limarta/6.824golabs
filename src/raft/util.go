35390fceda3ec0b01ad20923f4de6a6ba29630c3
Ian Limarta <limarta@mit.edu>
2022-04-09 03:02:45 -0400

    Added more debuging to util.go

--- src/raft/util.go
+++ src/raft/util.go
@@ -24,7 +24,7 @@ const (
 	dAppendListen    logTopic = "AL"
 	dDemote          logTopic = "DEMOTE"
 	dCommit          logTopic = "COMMIT"
-	dApply           logTopic = "APPLY"
+	dApply           logTopic = "APPLY_R"
 	dLoss            logTopic = "LOST"
 	dLogs            logTopic = "LOGS"
 	dStartAccept     logTopic = "START"
@@ -37,15 +37,16 @@ const (
 	dPersist         logTopic = "PERSIST"
 	dConflict        logTopic = "CONFLICT"
 	dStale           logTopic = "STALE"
-	dSnapshot        logTopic = "SNAP"
+	dSnapshot        logTopic = "SNAP_R"
 	dSnapshotApplied logTopic = "SNAPAPP"
 	dInstall         logTopic = "INSTALL"
 	dSearchCommit    logTopic = "SEARCH"
-	dCut             logTopic = "CUT"
+	dCut             logTopic = "CUT_R"
 )
 
 var debug_2 map[logTopic]int = map[logTopic]int{dStart: 1, dWon: 1, dSearchCommit: 1, dPersist: 1, dApply: 1, dConflict: 1}
 var debug_4 map[logTopic]int = map[logTopic]int{dCut: 1, dInstall: 1, dSnapshot: 1}
+var debug_5 map[logTopic]int = map[logTopic]int{dCut: 1, dInstall: 1, dSnapshot: 1, dApply: 1, dStart: 1, dAppend: 1}
 
 // Debugging
 const Debug = false
@@ -53,7 +54,7 @@ const Debug = false
 var debugVerbosity int
 var debugStart time.Time
 
-func setVerbosity() int {
+func SetVerbosity() int {
 	v := os.Getenv("VERBOSE")
 	level := 0
 	if v != "" {
@@ -64,7 +65,7 @@ func setVerbosity() int {
 		}
 	}
 	debugVerbosity = level
-	fmt.Println("VERBOSITY: ", level)
+	fmt.Println("RAFT VERBOSITY: ", level)
 	debugStart = time.Now()
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 	return level
@@ -79,7 +80,7 @@ func DPrintf(dTopic logTopic, format string, a ...interface{}) (n int, err error
 	time := time.Since(debugStart).Microseconds()
 	prefix := fmt.Sprintf("%06d %-7v ", time, string(dTopic))
 	format = prefix + format
-	if debugVerbosity == 1 {
+	if debugVerbosity == 4 {
 		log.Printf(format, a...)
 	} else if debugVerbosity == 2 {
 		if _, ok := debug_2[dTopic]; ok {
@@ -89,8 +90,8 @@ func DPrintf(dTopic logTopic, format string, a ...interface{}) (n int, err error
 		if dTopic == dPersist || dTopic == dRead {
 			log.Printf(format, a...)
 		}
-	} else if debugVerbosity == 4 {
-		if _, ok := debug_4[dTopic]; ok {
+	} else if debugVerbosity == 1 {
+		if _, ok := debug_5[dTopic]; ok {
 			log.Printf(format, a...)
 		}
 	}
